4782c6ebd0620f871c56af7979442f448628c56c
Stefano Macconi <stefano.macconi@gmail.com>
2019-02-11 14:39:50 +0100

    config

--- src/main.js
+++ src/main.js
@@ -7,7 +7,9 @@ import store from './store/store'
 import router from './router'
 import axios from 'axios'
 
-axios.defaults.baseURL = 'http://localhost:8080/spweb/rest'
+// axios.defaults.baseURL = 'http://localhost:8080/spweb/rest'
+// axios.defaults.baseURL = 'https://apps.molteniinformatica.com/spweb/rest'
+axios.defaults.baseURL = 'https://ditech.mflgroup.com/spweb/rest'
 
 new Vue({
   render: h => h(App),
